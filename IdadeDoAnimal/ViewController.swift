f5a023f232900640a7c24a2ac51451f8a3deea3b
Luis Andre de Lima <luislimamn@gmail.com>
2019-06-03 16:10:17 -0300

    Initial Commit

---
+++ IdadeDoAnimal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdadeDoAnimal
+//
+//  Created by Luis Lima on 03/06/2019.
+//  Copyright © 2019 Andr3ziim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
